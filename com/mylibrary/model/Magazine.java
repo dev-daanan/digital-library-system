69849e679ca0f5a9f999798127e6f301f019cd6f
Daanan Dasilva <daanandasilva@gmail.com>
2023-10-04 12:48:57 -0600

    User and Magazine Classes updated

--- com/mylibrary/model/Magazine.java
+++ com/mylibrary/model/Magazine.java
@@ -1,4 +1,13 @@
 package com.mylibrary.model;
 
-public class Magazine {
+public class Magazine extends Book {
+
+    private String magazineName;
+    private String issueMonth;
+
+    public Magazine(String title, String author, String ISBN, String genre, String status, String magazineName, String issueMonth) {
+        super(title, author, ISBN, genre, status);
+        this.magazineName = magazineName;
+        this.issueMonth = issueMonth;
+    }
 }
--- com/mylibrary/model/User.java
+++ com/mylibrary/model/User.java
@@ -1,7 +1,43 @@
 package com.mylibrary.model;
 
+import java.util.ArrayList;
+import java.util.Objects;
+
 public class User {
-    private String name;
-    private int id;
-    private boolean booksTakenOut;
+    private String userName;
+    private String userID;
+    private ArrayList<Book> checkedOutBooks;
+    private ArrayList<String> subscriptions;
+
+    public User(String userName, String userID, ArrayList<Book> checkedOutBooks, ArrayList<String> subscriptions) {
+        this.userName = userName;
+        this.userID = userID;
+        this.checkedOutBooks = checkedOutBooks;
+        this.subscriptions = subscriptions;
+    }
+
+    public boolean checkoutBook(Book book) { // Allows the user to check out a book.
+        if (checkedOutBooks.contains(book) ||
+                Objects.equals(book.getStatus(), "Checked out") ||
+                Objects.equals(book.getStatus(), "Reserved")) {
+            return false;
+        }
+        checkedOutBooks.add(book);
+        book.checkoutBook();
+        return true;
+    }
+
+    public Book returnBook(Book book) { // Allows the user to return a book.
+        checkedOutBooks.remove(book);
+        return book;
+
+    }
+
+    public void viewCheckedOutBooks() {
+        System.out.println("Checked out books:");
+        for (Book book : checkedOutBooks) {
+            System.out.println(book);
+        }
+    }
+
 }
